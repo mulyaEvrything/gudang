ec374ea21e0da71f6d8e09f90c72251553d8d255
mulyaEvrything <E020321045@mahasiswa.poliban.ac.id>
2024-04-22 03:51:35 -0600

    upload pertama

---
+++ database/gudang.sql
@@ -0,0 +1,314 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2024 at 11:45 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gudang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `id_barang` varchar(5) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jenis` int(11) NOT NULL,
+  `stok_minimum` int(11) NOT NULL,
+  `stok` int(11) NOT NULL DEFAULT 0,
+  `satuan` int(11) NOT NULL,
+  `foto` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_barang`
+--
+
+INSERT INTO `tbl_barang` (`id_barang`, `nama_barang`, `jenis`, `stok_minimum`, `stok`, `satuan`, `foto`) VALUES
+('B0001', 'Yamalub', 1, 10, 1200, 2, NULL),
+('B0002', 'AHM OIL', 1, 5, 33, 2, NULL),
+('B0003', 'Iconic Plus', 1, 10, 23, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_barang_keluar`
+--
+
+CREATE TABLE `tbl_barang_keluar` (
+  `id_transaksi` varchar(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `barang` varchar(5) NOT NULL,
+  `jumlah` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_barang_keluar`
+--
+
+INSERT INTO `tbl_barang_keluar` (`id_transaksi`, `tanggal`, `barang`, `jumlah`) VALUES
+('TK-0000001', '2024-04-19', 'B0002', 12),
+('TK-0000002', '2024-04-22', 'B0001', 10),
+('TK-0000004', '2024-04-22', 'B0003', 100);
+
+--
+-- Triggers `tbl_barang_keluar`
+--
+DELIMITER $$
+CREATE TRIGGER `hapus_stok_keluar` BEFORE DELETE ON `tbl_barang_keluar` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET stok=stok+OLD.jumlah
+WHERE id_barang=OLD.barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `stok_keluar` AFTER INSERT ON `tbl_barang_keluar` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET stok=stok-NEW.jumlah
+WHERE id_barang=NEW.barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_barang_masuk`
+--
+
+CREATE TABLE `tbl_barang_masuk` (
+  `id_transaksi` varchar(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `barang` varchar(5) NOT NULL,
+  `jumlah` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_barang_masuk`
+--
+
+INSERT INTO `tbl_barang_masuk` (`id_transaksi`, `tanggal`, `barang`, `jumlah`) VALUES
+('', '1970-01-01', '', 10),
+('TM-0000001', '2024-04-15', 'B0001', 10),
+('TM-0000003', '2024-04-16', 'B0002', 20),
+('TM-0000005', '2024-04-16', 'B0002', 20),
+('TM-0000006', '2024-04-16', 'B0001', 100),
+('TM-0000007', '2024-04-16', 'B0001', 1000),
+('TM-0000008', '2024-04-22', 'B0001', 100),
+('TM-0000009', '2024-04-22', 'B0003', 123),
+('TM-0000010', '2024-04-22', 'B0002', 2),
+('TM-0000011', '2024-04-22', 'B0002', 3);
+
+--
+-- Triggers `tbl_barang_masuk`
+--
+DELIMITER $$
+CREATE TRIGGER `hapus_stok_masuk` BEFORE DELETE ON `tbl_barang_masuk` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET stok=stok-OLD.jumlah
+WHERE id_barang=OLD.barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `stok_masuk` AFTER INSERT ON `tbl_barang_masuk` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET stok=stok+NEW.jumlah
+WHERE id_barang=NEW.barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_detail_invoice`
+--
+
+CREATE TABLE `tbl_detail_invoice` (
+  `id_invoice` int(11) NOT NULL,
+  `kode_barang` varchar(50) NOT NULL,
+  `barang` int(11) NOT NULL,
+  `harga_satuan` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_invoice`
+--
+
+CREATE TABLE `tbl_invoice` (
+  `id_invoice` int(10) NOT NULL,
+  `tgl_faktur` date NOT NULL,
+  `tgl_jatuh_tempo` date NOT NULL,
+  `no_po` varchar(50) NOT NULL,
+  `no_faktur` varchar(50) NOT NULL,
+  `kepada_yth` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_jenis`
+--
+
+CREATE TABLE `tbl_jenis` (
+  `id_jenis` int(11) NOT NULL,
+  `nama_jenis` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_jenis`
+--
+
+INSERT INTO `tbl_jenis` (`id_jenis`, `nama_jenis`) VALUES
+(1, 'Oli');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_satuan`
+--
+
+CREATE TABLE `tbl_satuan` (
+  `id_satuan` int(11) NOT NULL,
+  `nama_satuan` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_satuan`
+--
+
+INSERT INTO `tbl_satuan` (`id_satuan`, `nama_satuan`) VALUES
+(1, 'IBC'),
+(2, 'Drum'),
+(3, 'Pail');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(30) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `hak_akses` enum('Administrator','Admin Gudang','Kepala Gudang') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `password`, `hak_akses`) VALUES
+(1, 'Admin', 'administrator', '$2y$12$Yi/I5f1jPoQNQnh6lWoVfuz.RtZ3OHcKN6PU.I62P0fYK1tJ7xMRi', 'Administrator'),
+(2, 'Admin Gudang', 'admin gudang', '$2y$12$BeRYh13zfPXej97VgcfeNucYJGTElha5sRyIUQm1278D2u2Aqf6DS', 'Admin Gudang');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `tbl_barang_keluar`
+--
+ALTER TABLE `tbl_barang_keluar`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `tbl_barang_masuk`
+--
+ALTER TABLE `tbl_barang_masuk`
+  ADD PRIMARY KEY (`id_transaksi`);
+
+--
+-- Indexes for table `tbl_detail_invoice`
+--
+ALTER TABLE `tbl_detail_invoice`
+  ADD PRIMARY KEY (`id_invoice`);
+
+--
+-- Indexes for table `tbl_invoice`
+--
+ALTER TABLE `tbl_invoice`
+  ADD PRIMARY KEY (`id_invoice`);
+
+--
+-- Indexes for table `tbl_jenis`
+--
+ALTER TABLE `tbl_jenis`
+  ADD PRIMARY KEY (`id_jenis`);
+
+--
+-- Indexes for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_detail_invoice`
+--
+ALTER TABLE `tbl_detail_invoice`
+  MODIFY `id_invoice` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_invoice`
+--
+ALTER TABLE `tbl_invoice`
+  MODIFY `id_invoice` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_jenis`
+--
+ALTER TABLE `tbl_jenis`
+  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ modules/barang-masuk/print.php
@@ -0,0 +1,89 @@
+<style>
+  .kop {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+</style>
+<?php
+session_start();      // mengaktifkan session
+
+// pengecekan session login user 
+// jika user belum login
+if (empty($_SESSION['username']) && empty($_SESSION['password'])) {
+  // alihkan ke halaman login dan tampilkan pesan peringatan login
+  header('location: ../../login.php?pesan=2');
+}
+// jika user sudah login, maka jalankan perintah untuk export
+else {
+  // panggil file "database.php" untuk koneksi ke database
+  require_once "../../config/database.php";
+
+  // fungsi header untuk mengirimkan raw data excel
+?>
+  <!-- halaman HTML yang akan diexport ke excel -->
+  <!-- judul tabel -->
+  <div class="kop">
+    <div class="left">
+      <h4>PT. LUMASINDO SUMBER ABADI</h4>
+      <h4>AUTHORIZED DISTRIBUTOR REPSOL</h4>
+      <p>Jl. Yogyakarta No. 15 RT.001 RW.006, Loktabat Selatan, Banjar Baru Selatan</p>
+      <p>Email: lunasindo_sa@yahoo.com</p>
+      <p>TGL: <?= date("Y-m-d H:i:s") ?></p>
+      <p>NO PO: Pemakaian Oil tanggal 16 Februari - 29 Februari 2024</p>
+    </div>
+    <div class="right">
+      <h4>SURAT JALAN</h4>
+      <p>No: 87215981725</p>
+      <p>Kepada Yth</p>
+      <p>PT. Anugrah Energi Kalimantan</p>
+      <p>Citraland Banjarmasin, Comercial I-WALK</p>
+      <p>Komplek manhatan No. 48 Kab. Banjar</p>
+    </div>
+  </div>
+  <!-- tabel untuk menampilkan data dari database -->
+  <table border="1">
+    <thead>
+      <tr>
+        <th height="30" align="center" vertical="center">No.</th>
+        <th height="30" align="center" vertical="center">Kode Barang</th>
+        <th height="30" align="center" vertical="center">Nama Barang</th>
+        <th height="30" align="center" vertical="center">Jumlah</th>
+        <th height="30" align="center" vertical="center">Satuan</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      // variabel untuk nomor urut tabel 
+      $no = 1;
+      $jumlah = 0;
+      // sql statement untuk menampilkan data dari tabel "tbl_jenis"
+      $query = mysqli_query($mysqli, "SELECT *
+      FROM tbl_barang_masuk INNER JOIN tbl_barang ON tbl_barang.id_barang = tbl_barang_masuk.barang
+      INNER JOIN tbl_satuan ON tbl_satuan.id_satuan = tbl_barang.satuan
+      WHERE tbl_barang_masuk.tanggal = (SELECT MAX(tanggal) FROM tbl_barang_masuk)")
+                                      or die('Ada kesalahan pada query tampil data : ' . mysqli_error($mysqli));
+      // ambil data hasil query
+      while ($data = mysqli_fetch_assoc($query)) { 
+        $jumlah += $data['jumlah'];
+        ?>
+        <!-- tampilkan data -->
+        <tr>
+          <td width="70" align="center"><?php echo $no++; ?></td>
+          <td width="500"><?= $data['id_barang'] ?></td>
+          <td width="500"><?= $data['nama_barang'] ?></td>
+          <td width="500"><?= $data['jumlah'] ?></td>
+          <td width="500"><?= $data['nama_satuan'] ?></td>
+        </tr>
+      <?php } ?>
+      <tr>
+        <td height="30" align="center" vertical="center" colspan="3">Jumlah</td>
+        <td width="500" align="center" colspan="2"><?= $jumlah ?></td>
+      </tr>
+    </tbody>
+  </table>
+<?php } ?>
+
+<script>
+    window.print();
+</script>
