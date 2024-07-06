7372b4fcf3d32b094efc0c1f6b402fef04c3d0c7
mulyaEvrything <E020321045@mahasiswa.poliban.ac.id>
2024-07-06 10:59:53 -0600

    Update

--- modules/laporan-stok/tampil_data.php
+++ modules/laporan-stok/tampil_data.php
@@ -139,10 +139,15 @@ else { ?>
                 $no = 1;
 
                 // sql statement untuk menampilkan data dari tabel "tbl_barang_masuk", tabel "tbl_barang", dan tabel "tbl_satuan" berdasarkan "tanggal"
-                $query = mysqli_query($mysqli, "SELECT a.id_transaksi, a.tanggal, a.barang, a.jumlah, b.nama_barang, c.nama_satuan
-                                                FROM tbl_barang_masuk as a INNER JOIN tbl_barang as b INNER JOIN tbl_satuan as c 
-                                                ON a.barang=b.id_barang AND b.satuan=c.id_satuan 
-                                                WHERE a.tanggal BETWEEN '$tanggal' ORDER BY a.id_transaksi ASC")
+                $query = mysqli_query($mysqli, "SELECT *,
+                                      IFNULL((SELECT SUM(jumlah) FROM tbl_detail_barang_masuk dbm, tbl_barang_masuk bm WHERE dbm.id_barang = b.id_barang
+                                            AND dbm.id_masuk = bm.id_transaksi AND bm.tanggal <= '$tanggal'),0) as jlh_masuk,
+                                      IFNULL((SELECT SUM(jumlah) FROM tbl_detail_barang_keluar dbk, tbl_barang_keluar bk WHERE dbk.id_barang = b.id_barang
+                                            AND dbk.id_keluar = bk.id_transaksi AND bk.tanggal <= '$tanggal'),0) as jlh_keluar,
+                                      (SELECT jlh_masuk - jlh_keluar) as stoknow
+                                      FROM tbl_barang b
+                                      LEFT JOIN tbl_satuan s ON s.id_satuan = b.satuan
+                                      ")
                                                 or die('Ada kesalahan pada query tampil data : ' . mysqli_error($mysqli));
                 // ambil data hasil query
                 while ($data = mysqli_fetch_assoc($query)) { ?>
